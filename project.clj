5a40c1849873b978a4f5b54cf58bae4096b863a4
Jonathan Halterman <jhalterman@gmail.com>
2014-01-07 13:45:11 -0800

    Update amqp-client dep

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/jhalterman/snare"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"] 
-                 [com.rabbitmq/amqp-client "3.1.4"] 
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.rabbitmq/amqp-client "3.2.1"]
                  [org.clojure/tools.cli "0.2.4"]]
   :uberjar-name "snare.jar"
   :main snare.core)
